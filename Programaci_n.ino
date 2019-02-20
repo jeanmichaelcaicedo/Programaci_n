8909fc2b17bec14f901548f12f541f98225fc326
javi <javia.alzatet@uqvirtual.edu.co>
2019-02-20 12:59:46 -0500

    borré lo inncesario

--- Programaci_n.ino
+++ Programaci_n.ino
@@ -13,26 +13,4 @@ SoftwareSerial BT(Rxbluetooh, Txbluetooh);//pines del bluetooh.
 char orden;
  Serial.begin(9600);
 
-  // Provide ground and power by using the analog inputs as normal digital pins.
-  // This makes it possible to directly connect the breakout board to the
-  // Arduino. If you use the normal 5V and GND pins on the Arduino,
-  // you can remove these lines.
-  pinMode(groundpin, OUTPUT);
-  pinMode(powerpin, OUTPUT);
-  digitalWrite(groundpin, LOW);
-  digitalWrite(powerpin, HIGH);
-}
-
-void loop() {
-  // print the sensor values:
-  Serial.print(analogRead(xpin));
-  // print a tab between values:
-  Serial.print("\t");
-  Serial.print(analogRead(ypin));
-  // print a tab between values:
-  Serial.print("\t");
-  Serial.print(analogRead(zpin));
-  Serial.println();
-  // delay before next reading:
-  delay(100);
-}
+  
