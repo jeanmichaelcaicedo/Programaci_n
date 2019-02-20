3fdcc68d23b055f11380d5745dc4d1a5b7c53fc3
javi <javia.alzatet@uqvirtual.edu.co>
2019-02-20 14:06:33 -0500

    se añadio todo el codigo de programacion

--- Programaci_n.ino
+++ Programaci_n.ino
@@ -11,6 +11,84 @@ Servo ServoDE,ServoIZQ;
 //da los nombres a cada servo
 SoftwareSerial BT(Rxbluetooh, Txbluetooh);//pines del bluetooh.
 char orden;
- Serial.begin(9600);
 
-  
+void setup() 
+{
+  pinMode (PINSERVODERECHO,OUTPUT);
+  pinMode (PINSERVOIZQUIERDO,OUTPUT);
+  ServoDE.attach(PINSERVODERECHO);
+  ServoIZQ.attach(PINSERVOIZQUIERDO);
+  Serial.begin(9600);
+  BT.begin(9600);
+
+}
+
+void loop() 
+{
+  if(BT.available()>0)
+  {
+    orden=BT.read();
+    BT.println(orden);
+  }
+  if(Serial.available() > 0)
+  {
+    int orden=Serial.read();
+    Serial.println(orden);
+
+    switch(orden)//orden es la variable
+    {
+      case 'w': case 'W': //avanza 
+      ServoDE.write(0);
+      ServoIZQ.write(180);
+      Serial.println("avanzo");
+      break;
+
+      case 's': case 'S': //retrocede
+      ServoDE.write(180);
+      ServoIZQ.write(0);
+      Serial.println("retrocedo");
+      break;
+
+      case 'a': case 'A': //gira a la izquierda
+      ServoDE.write(0);
+      ServoIZQ.write(90);
+      Serial.println("giro a la izquierda");
+      break;
+
+      case 'd': case 'D': //gira a la derecha
+      ServoDE.write(90);
+      ServoIZQ.write(180);
+      Serial.println("giro a la derecha");
+      break;
+
+      case 'e': case 'E': //detenerse
+      ServoDE.write(90);
+      ServoIZQ.write(90);
+      Serial.println("me detengo");
+      break;
+
+      case 'p': case 'P': //muestra
+      ServoDE.write(0);
+      ServoIZQ.write(180);
+      delay(1000);
+      ServoDE.write(180);
+      ServoIZQ.write(0);
+      delay(1000);
+      ServoDE.write(90);
+      ServoIZQ.write(90);
+      Serial.println("cuchau");
+      break;
+
+      case 'L': case 'l':
+      ServoDE.write(0);
+      ServoIZQ.write(90);
+      delay(3000);
+      ServoDE.write(90);
+      ServoIZQ.write(180);
+      delay(3000);
+      ServoDE.write(0);
+      ServoIZQ.write(180);
+      break;
+    }
+  }
+}
